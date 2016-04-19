f7583c116496946e9d5d7d8ba78482bd58f72df3
Hiroyuki Sano <sh19910711@gmail.com>
2016-04-19 13:07:22 +0900

    wip

--- lib/contest/codeforces.rb
+++ lib/contest/codeforces.rb
@@ -3,17 +3,22 @@ require "codeforces"
 module Contest
   class Codeforces
     def contests(t = 0)
-      ::Codeforces.contests.map do |c|
-        {
-          :contest => "Codeforces",
-          :contest_short => "CF",
-          :name => c.name,
-          :start_time_sec => c.startTimeSeconds,
-          :duration_sec => c.durationSeconds,
-        }
-      end.select do |c|
+      all_contests.select do |c|
         c[:start_time_sec] >= t
       end
     end
+
+    private
+      def all_contests
+        ::Codeforces.contests.map do |c|
+          {
+            :contest => "Codeforces",
+            :contest_short => "CF",
+            :name => c.name,
+            :start_time_sec => c.startTimeSeconds,
+            :duration_sec => c.durationSeconds,
+          }
+        end
+      end
   end
 end
