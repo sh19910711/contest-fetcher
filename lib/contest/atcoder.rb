97519f8ecf948e2047696ca015fd06069c955854
Hiroyuki Sano <sh19910711@gmail.com>
2016-04-19 14:22:09 +0900

    wip

--- lib/contest/atcoder.rb
+++ lib/contest/atcoder.rb
@@ -4,11 +4,15 @@ require "contest/utils/google_calendar"
 module Contest
   class AtCoder < Utils::GoogleCalendar
     include Mixins::Findable
-    def contests
+
+    def contests(t = 0)
       regulars = 'atcoder.jp_gqd1dqpjbld3mhfm4q07e4rops@group.calendar.google.com'
-      others = 'atcoder.jp_drp3qk1qgpb84vcdj418fsbo7k@group.calendar.google.com'
+      others   = 'atcoder.jp_drp3qk1qgpb84vcdj418fsbo7k@group.calendar.google.com'
+
       items(regulars).concat(items(others)).map do |item|
         { :contest => "AtCoder" }.merge(item)
+      end.select do |item|
+        item[:start_time_sec] >= t
       end
     end
   end
--- spec/atcoder_spec.rb
+++ spec/atcoder_spec.rb
@@ -3,18 +3,35 @@ require "spec_helper"
 vcr_opts = { :cassette_name => "atcoder" }
 describe Contest::AtCoder, :vcr => vcr_opts do
   describe :contests do
-    subject { fetcher.contests }
-    it { expect(subject.map {|contest| contest[:name] }).to include /AtCoder Regular/ }
-    it { expect(subject.map {|contest| contest[:name] }).to include /東京大学プログラミングコンテスト/ }
+    subject do
+      fetcher.contests.map {|contest| contest[:name] }
+    end
+    it { should include /AtCoder Regular/ }
+    it { should include /東京大学プログラミングコンテスト/ }
+
+    context :before_arc016 do
+      subject do
+        fetcher.contests(arc016_start-1).map {|contest| contest[:name] }
+      end
+      it { should include /AtCoder Regular Contest #016/ }
+    end
+
+    context :after_arc016 do
+      subject do
+        fetcher.contests(arc016_start+1).map {|contest| contest[:name] }
+      end
+      it { should_not include /AtCoder Regular Contest #016/ }
+    end
   end
 
   describe :arc016 do
     subject { fetcher.find_first "AtCoder Regular Contest #016" }
     it { expect(subject[:name]).to eq "AtCoder Regular Contest #016" }
     it { expect(subject[:contest_short]).to be nil }
-    it { expect(subject[:start_time_sec]).to eq DateTime.parse("2013/11/04 21:00:00 JST").to_time.to_i}
+    it { expect(subject[:start_time_sec]).to eq arc016_start }
   end
 
   include Helpers::GAPI
   let(:fetcher) { Contest::AtCoder.new(gapi_client_email, gapi_private_key, gapi_private_pass) }
+  let(:arc016_start) { DateTime.parse("2013/11/04 21:00:00 JST").to_time.to_i }
 end
